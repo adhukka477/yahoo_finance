e8853f5987fd49c945ceb3c61ce6c81b6e43d5ef
adhukka477 <alishandhukka@gmail.com>
2023-01-02 14:47:22 -0600

    packaged the ticker class for use in other projects

---
+++ setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name='yahoo_finance',
+    version='0.0.1',
+    author='Alishan Dhukka',
+    author_email='alishandhukka@gmail.com',
+    description='Package to scrape stock price history from yahoo finance',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url='https://github.com/mike-huls/toolbox',
+    project_urls = {
+        "Bug Tracker": "https://github.com/adhukka477/yahoo_finance"
+    },
+    license='MIT',
+    packages=['yahoo_finance'],
+    install_requires=['pandas', 'datetime', 'urllib', 'bs4'],
+)
---
+++ tickers.py
@@ -0,0 +1,75 @@
+# import necessary libraries
+
+import pandas as pd
+from datetime import datetime as dt
+from urllib.request import Request, urlopen 
+from bs4 import BeautifulSoup
+import io
+
+
+class Ticker():
+  
+    def __init__(self, ticker, start = "1970-01-02", end = dt.strftime(dt.now(), "%Y-%m-%d"), interval = 'd'):
+
+        self.ticker = ticker
+        self.interval = interval
+        self.start = int(dt.timestamp(dt.strptime(start, "%Y-%m-%d")))
+        self.end = int(dt.timestamp(dt.strptime(end, "%Y-%m-%d")))
+
+
+        if str(self.interval).lower() == 'd':
+            self.getDailyData()
+        elif str(self.interval).lower() == 'm':
+            self.getMonthlyData()
+        elif str(self.interval).lower() == 'w':
+            self.getWeeklyData()
+
+    # get stock prices using yfinance library
+    def getDailyData(self):
+
+        url = "https://query1.finance.yahoo.com/v7/finance/download/" + self.ticker +\
+              "?period1=" + str(self.start)+\
+              "&period2=" + str(self.end)+\
+              "&interval=1d"+\
+              "&events=history&includeAdjustedClose=true"
+
+        req = Request(url, headers={'User-Agent': 'Mozilla/5.0'})
+        html = urlopen(req)
+        soup = BeautifulSoup(html, "html.parser")
+        self.df = pd.read_csv(io.StringIO(soup.text), sep=",")
+
+        self.df = self.df.loc[:,['Date', 'Open', 'High', 'Low', 'Close', "Volume"]]
+        self.df.sort_values("Date", inplace=True)
+    
+    def getWeeklyData(self):
+
+        url = "https://query1.finance.yahoo.com/v7/finance/download/" + self.ticker +\
+              "?period1=" + str(self.start)+\
+              "&period2=" + str(self.end)+\
+              "&interval=1wk"+\
+              "&events=history&includeAdjustedClose=true"
+
+        req = Request(url, headers={'User-Agent': 'Mozilla/5.0'})
+        html = urlopen(req)
+        soup = BeautifulSoup(html, "html.parser")
+        self.df = pd.read_csv(io.StringIO(soup.text), sep=",")
+
+        self.df = self.df.loc[:,['Date', 'Open', 'High', 'Low', 'Close', "Volume"]]
+        self.df.sort_values("Date", inplace=True)
+    
+    def getMonthlyData(self):
+
+        url = "https://query1.finance.yahoo.com/v7/finance/download/" + self.ticker +\
+              "?period1=" + str(self.start)+\
+              "&period2=" + str(self.end)+\
+              "&interval=1mo"+\
+              "&events=history&includeAdjustedClose=true"
+
+        req = Request(url, headers={'User-Agent': 'Mozilla/5.0'})
+        html = urlopen(req)
+        soup = BeautifulSoup(html, "html.parser")
+        self.df = pd.read_csv(io.StringIO(soup.text), sep=",")
+
+        self.df = self.df.loc[:,['Date', 'Open', 'High', 'Low', 'Close', "Volume"]]
+        self.df.sort_values("Date", inplace=True)
+
